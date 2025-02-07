813a0847ed1184b1b2db80e194df001d4d010097
Lindsey Welton <10878168@uvu.edu>
2025-02-07 14:26:33 -0700

    Added x and z movement on challenge two

--- LindseyScripting2025/Assets/Scripts/TransformController.cs
+++ LindseyScripting2025/Assets/Scripts/TransformController.cs
@@ -5,12 +5,12 @@ public class TransformController : MonoBehaviour
     private void Update()
     {
         //Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
+        var x = Mathf.PingPong(Time.time, 3); transform.position = new Vector3(x, 0, 0);
         var y = Mathf.PingPong(Time.time, 3);
         var p = new Vector3(y, x, 0);
         transform.position = p;
 
     // Rotate the target GameObject
-    transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    transform.Rotate(new Vector3(30, 30, 0) * Time.deltaTime);
     }
 }
