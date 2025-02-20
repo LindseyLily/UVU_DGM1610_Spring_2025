16fffa454da5e47ef4fffadd88151d729cf29190
Lindsey Welton <10878168@uvu.edu>
2025-02-20 14:40:39 -0700

    Lab 6 Finished
    - Enemy damage system applied to saw
    - No notifications of player HP yet
    - Lab 6 copy added into Scripting 6 file

--- LindseyScripting2025/Assets/Scripts/Saw.cs
+++ LindseyScripting2025/Assets/Scripts/Saw.cs
@@ -2,5 +2,18 @@ using UnityEngine;
 
 public class Saw : MonoBehaviour
 {
-    public
+    //amount of damage done each hit
+    public int damage = 5;
+    
+    // Collision trigger
+    private void OnCollisionEnter(Collision other)
+    {
+        // Check if the player has a health system
+        if (other.gameObject.TryGetComponent(out HealthSystem healthSystem))
+        {
+            //Deals damage
+            healthSystem.TakeDamage(damage);
+            Debug.Log("Damage dealt:" + damage);
+        }
+    }
 }
