08c0ceef25fed16b9e3245d426b9415805328ac6
Lindsey Welton <10878168@uvu.edu>
2025-03-06 14:42:25 -0700

    Lab 8
    - SimpleIntData + public SimpleTMPB line of code with the TMPFruit score tracker would visibly update when the player gets another fruit
    - Players HP resets after every play

--- LindseyScripting2025/Assets/Scripts/HealthContainer.cs
+++ LindseyScripting2025/Assets/Scripts/HealthContainer.cs
@@ -4,6 +4,12 @@ public class HealthContainer : MonoBehaviour
 {
  public SimpleFloatData healthData;
 
+ private void Start()
+ {
+  //Resets HP to 1 when the game starts
+  healthData.SetValue(1f);
+ }
+ 
  public void ReduceHealth(float amount)
  {
   healthData.UpdateValue(amount);
--- LindseyScripting2025/Assets/Scripts/SimpleIntData.cs
+++ LindseyScripting2025/Assets/Scripts/SimpleIntData.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class SimpleIntData : MonoBehaviour
 {
     public int value;
+    // Will link to the score text
+    public SimpleTextMeshProBehaviour textMeshProBehaviour;
 
     public void UpdateValue(int amount)
     {
         value += amount;
+        // Updates the UI value after it changes
+        textMeshProBehaviour.UpdateWithIntData();
     }
 
     public void SetValue(int amount)
     {
         value = amount;
+        //Updates the UI after the value is set
+        textMeshProBehaviour.UpdateWithIntData();
     }
 }
