7dde8936abe7f6ec73eb78aa31a745b804bce1cb
Lindsey Welton <10878168@uvu.edu>
2025-03-14 16:38:31 -0600

    Lab 9
    - Audio script made

---
+++ LindseyScripting2025/Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+    }
+}
