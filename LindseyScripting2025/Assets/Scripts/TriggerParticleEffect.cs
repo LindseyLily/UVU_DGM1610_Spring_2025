d83686d02420129bb320a8ef9c21053cfaa09e47
VineLilyLindsey <10878168@uvu.edu>
2025-04-07 15:25:45 -0600

    Challenge 11
    - script edited and updated for bursts

--- LindseyScripting2025/Assets/Scripts/TriggerParticleEffect.cs
+++ LindseyScripting2025/Assets/Scripts/TriggerParticleEffect.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using System.Collections;
+
 [RequireComponent(typeof(ParticleSystem), typeof(Collider))]
 public class TriggerParticleEffect : MonoBehaviour
 {
     //Particle system
     private ParticleSystem particleSystem;
-    // Amount of particles
-    public int particleAmount = 10;
+
+    public int firstEmissionAmount = 10;
+    public int secondEmissionAmount = 20;
+    public int thirdEmissionAmount = 30;
+    public float delayBetweenEmissions = 0.1f; // Delay time between emissions
 
     private void Start()
     {
@@ -17,8 +22,22 @@ public class TriggerParticleEffect : MonoBehaviour
         // Checks if the player has triggered the event
         if (other.gameObject.GetComponent<CharacterController>())
         {
-            //Emits the particles
-            particleSystem.Emit(particleAmount);
+            StartCoroutine(routine: EmitParticlesCoroutine());
         }
+    }
+    
+    // Coroutine to emit particles multiple times with delays
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        // First emission
+        particleSystem.Emit(firstEmissionAmount); // Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); // Wait for a specified time
+        
+        // Second emission
+        particleSystem.Emit(secondEmissionAmount);
+        yield return new WaitForSeconds(delayBetweenEmissions);
+        
+        // Third emission
+        particleSystem.Emit(thirdEmissionAmount);
     }
 }
